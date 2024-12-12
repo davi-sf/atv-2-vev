f2845732b24c9e559cbda5885f4a297a50652e87
davi-sf <davi.soares.ferreira@ccc.ufcg.edu.br>
2024-12-12 10:28:03 -0300

    inserscao da classe pagamento, mas com construtor vazio resultado em redbar ao tentar criar um pagamento

---
+++ processadorDeContas/src/model/Pagamento.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Pagamento {
+
+    private double valorPago;
+    private String dataPagamento;
+    private String tipoPagamento;
+
+    public Pagamento(double valorPago, String dataPagamento, String tipoPagamento) {
+
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public String getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(String dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+}
--- processadorDeContas/src/test/PagamentoTest.java
+++ processadorDeContas/src/test/PagamentoTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import model.Pagamento;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
