3f048cfe1ac1332115eb61d1c77f42a0a06fd0ae
davi-sf <davi.soares.ferreira@ccc.ufcg.edu.br>
2024-12-12 10:26:02 -0300

    criando testes para pagamento sem implementacao da clase pagamento

---
+++ processadorDeContas/src/test/PagamentoTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PagamentoTest {
+
+    @Test
+    public void testCriarPagamentoComDadosValidos() {
+    Pagamento pagamento = new Pagamento(100.00, "12-12-2024", "BOLETO");
+
+    double delta = 0.0001;
+
+
+    assertEquals(100.00, pagamento.getValorPago(), delta);
+    assertEquals("12-12-2024", pagamento.getDataPagamento());
+    assertEquals("BOLETO", pagamento.getTipoPagamento());
+}
+
+
+}
